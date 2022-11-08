e22fcf07a731ff1318e52e94cc29cddd88c06a0b
+ <+>
2022-11-08 15:26:01 +0300

    SLA-17 done

--- app/models/itemlist.rb
+++ app/models/itemlist.rb
@@ -1,4 +1,3 @@
 class ItemList < ActiveRecord::Base
-    belongs_to :shopping_lists
-    belongs_to :items
+    
 end
--- app/models/shoppinglist.rb
+++ app/models/shoppinglist.rb
@@ -1,3 +1,4 @@
 class shoppingList < ActiveRecord::Base
     has_many :items
+    puts "Hello"
 end
