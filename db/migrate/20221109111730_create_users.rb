4335db45db8637d51c2c16627ce6cc63934b322f
+ <+>
2022-11-09 16:08:03 +0300

    test

--- db/migrate/20221109111730_create_users.rb
+++ db/migrate/20221109111730_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      #Comment
+      #Comment ''
+      #New change
       t.string :first_name
       t.string :last_name
       t.string :email
