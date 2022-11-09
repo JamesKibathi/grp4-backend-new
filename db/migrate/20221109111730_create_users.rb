d1d33a13b3b05197856401b129e06e7f1d5ef18f
+ <+>
2022-11-09 14:49:34 +0300

    SLA-28 updated the shopping list table

--- db/migrate/20221109111730_create_users.rb
+++ db/migrate/20221109111730_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      # this is a table
-      #Confirmed change
       t.string :first_name
       t.string :last_name
       t.string :email
