75c6e98dec3228bf4980958ae380c92940c08e37
+ <+>
2022-11-09 14:43:20 +0300

    SLA-28 commented out the tables>

--- db/migrate/20221109111730_create_users.rb
+++ db/migrate/20221109111730_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       # this is a table
+      #Confirmed change
       t.string :first_name
       t.string :last_name
       t.string :email
