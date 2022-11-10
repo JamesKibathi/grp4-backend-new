ffc3e8913ab95a5b7e9e43b00808f49554e9bca5
+ <+>
2022-11-10 13:59:50 +0300

    added some routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    
+  get "/users" do
+    users = User.all 
+    users.to_json
   end
-
-  
 end
